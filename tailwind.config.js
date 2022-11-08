983d3fc68151ebd6d5543f0d64da3929ff16da48
Cornerstone <fortunecornerstone@gmail.com>
2022-11-08 22:12:44 +0100

    feat: Configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        card: "#1B1B1B",
+        text: "#999999",
+        yel: "#FFFF57",
+        body: "#222222",
+        border: "#CCCCCC",
+      },
+      borderRadius: { 20: "20px" },
+      maxWidth: {
+        app: "960px",
+      },
+    },
   },
   plugins: [],
-}
+};
